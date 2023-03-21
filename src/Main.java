28904d5853788c12254c4f83a059688e6e23447a
Vinicius Leme <viniciuslemesoares@gmail.com>
2023-03-21 16:30:52 -0300

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Cliente cliente1 = new Cliente("Vinicius", "52783362856", "09/04/2003", 19, "Campinas");
+        //Veiculo carro1 = new Veiculo("FAB-6086", "Hyundai", "HB20");
+        //Sinistro sinistro1 = new Sinistro("13/05/2023", "Valinhos");
+        //Seguradora seguradora1 = new Seguradora("PortoSeguro", "(19)999047277", "@email.com", "valinhos");
+    
+        cliente1.setCpf(cliente1.formatacaoCpf(cliente1.getCpf()));
+        System.out.println(cliente1.validaCpf(cliente1.getCpf()));
+    }
+
+    
+}
